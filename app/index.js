69ee1eca61c551eb81a4b309b681b3405f607cbc
ldandoy <ldandoy@overconsulting.net>
2022-11-07 16:52:20 +0100

    Ajout mongo

--- app/index.js
+++ app/index.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 
 const app = express();
 
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect('', {
+    useNewUrlParser: true
+}, (error) => {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log('BD connect !');
+    }
+})
 
 const classesRouter = require('./routes/classes');
 
